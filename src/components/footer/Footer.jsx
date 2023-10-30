beca9ba8c2b3ec38c0c46fe6dc23d5d51bf1beb3
asrorbek00 <aserravshanbekov@gmail.com>
2023-10-30 20:04:25 +0500

    t

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaTelegramPlane } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { RiInstagramFill } from "react-icons/ri";
+
+const Footer = () => {
+  return (
+    <div id="works" className=" mx-auto m-auto h-[300px]  mt-16 sm:h-[250px]">
+      <div className=" bg-yellow-400 h-full flex flex-col gap-8 items-center justify-between p-10 sm:p-7">
+        <h2 
+        data-aos="zoom-out"
+         className=" font-bold text-5xl sm:text-3xl">Let's Talk</h2>
+        <div className=" flex items-center justify-center gap-8 sm:gap-5">
+          <a 
+          data-aos="fade-up" data-aos-duration="1000"
+
+            href="https://t.me/aser1005"
+            className="box font-medium text-white   flex items-center justify-center flex-col"
+          >
+            <FaTelegramPlane className=" text-black text-3xl hover:scale-125 cursor-pointer" />
+            <p>Telegram</p>
+          </a>
+          <p>+998978320508</p>
+        </div>
+        <div className="sm:text-[12px]">
+          | Copyright &copy; <span>2023 asrorbek </span> All rights reserved
+          <a href="#"></a> |
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/rm0G30xw/Screen-Shot-2023-10-30-at-18-11-54.png',
+    title: "Mover movie ",
+    desc: "",
+    link: "https://chimerical-duckanoo-dac441.netlify.app/",
+    git: "https://github.com/asrorbek00/imtihon6.git",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/HLjb8cS9/Screen-Shot-2023-10-30-at-18-11-32.png',
+    title: "shop bucket",
+    link: "https://jocular-phoenix-e2d970.netlify.app/",
+    git: "https://github.com/asrorbek00/7.5-dars.git",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/gJFrcn1J/Screen-Shot-2023-10-30-at-18-10-52.png',
+    title: "Food ingredient",
+    desc: "",
+    link: "https://precious-platypus-42914d.netlify.app/",
+    git: "https://github.com/asrorbek00/6.11-dars.git",
+  }
+  
+];
